dd4a1b926d95e4f7e151f818f91d5881f428bcff
Ashley Dogan <a.a.rappette@gmail.com>
2025-01-28 01:57:38 +0000

    first day

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
-
+import "../../styles/index.css";
+import { renderToNodeStream } from "react-dom/server";
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-            
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+const Home = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const [todo, setTodo] = useState("");
+  return (
+    <div className="container justify-content-center">
+      <h1>to do list</h1>
+      <div className="container bg-light">
+        <div className="row">
+          <input
+            type="text"
+            placeholder="What needs to be done?"
+            onChange={(e) => setInputValue(e.target.value)}
+            value={inputValue}
+            onSubmit={(e) => {
+              setTodo(todo.concat(e.target.inputValue));
+            }}
+          ></input>
+        </div>
+      </div>
+      <div>number of tasks</div>
+    </div>
+  );
 };
 
 export default Home;
