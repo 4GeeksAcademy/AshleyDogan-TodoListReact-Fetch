2c415b07a3355efdf5d880b94adede3a52a4de44
Ashley Dogan <a.a.rappette@gmail.com>
2025-03-13 17:45:10 +0000

    project resubmit

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from "react";
-
-
 import "../../styles/index.css";
 
-
 const Home = () => {
-
   const [inputValue, setInputValue] = useState("");
   const [todo, setTodo] = useState([]);
 
@@ -19,7 +15,7 @@ const Home = () => {
       })
       .then((data) => setTodo(data))
       .catch((error) => console.error(error));
-  }, []); 
+  }, []);
 
   const addTask = (e) => {
     if (e.key === "Enter" && inputValue.trim() !== "") {
@@ -30,7 +26,10 @@ const Home = () => {
         body: JSON.stringify(newTask),
         headers: { "Content-Type": "application/json" },
       })
-        .then((response) => response.json())
+        .then((response) => {
+          if (!response.ok) throw new Error("Error adding task");
+          return response.json();
+        })
         .then((data) => {
           setTodo([...todo, data]);
           setInputValue("");
@@ -38,7 +37,7 @@ const Home = () => {
         .catch((error) => console.error(error));
     }
   };
-  
+
   const deleteTask = (taskId) => {
     fetch(`https://playground.4geeks.com/apis/todo/${taskId}`, {
       method: "DELETE",
@@ -50,7 +49,7 @@ const Home = () => {
         return response.json();
       })
       .then(() => {
-        setTodo(todo.filter((task) => task.id !== taskId)); 
+        setTodo(todo.filter((task) => task.id !== taskId));
       })
       .catch((error) => console.error(error));
   };
@@ -76,38 +75,36 @@ const Home = () => {
             type="text"
             onChange={(e) => setInputValue(e.target.value)}
             value={inputValue}
-            onKeyPress={(e) => {
-              if (e.key === "Enter") {
-                setTodo(todo.concat(inputValue));
-                setInputValue("");
-              }
-            }}
+            onKeyPress={addTask}
             placeholder="What needs to be done?"
           ></input>
         </div>
+
         {todo.map((item, index) => (
-          <div className="inline-row">
+          <div className="inline-row" key={item.id}>
             <div id="hoverText">
-              {item}{" "}
+              {item.label}{" "}
               <div id="hidden">
                 <i
-                  class="fa-solid fa-x"
-                  onClick={() =>
-                    setTodo(
-                      todo.filter((t, currentIndex) => index != currentIndex)
-                    )
-                  }
+                  className="fa-solid fa-x"
+                  onClick={() => deleteTask(item.id)}
                 ></i>
               </div>
             </div>
           </div>
         ))}
       </div>
+
       <div className="footer">
-        {todo.length !== 0 ? "" : "no tasks - add a task"}
+        {todo.length !== 0 ? (
+          <button onClick={clearTasks}>Clear All</button>
+        ) : (
+          "No tasks - add a task"
+        )}
       </div>
     </div>
   );
 };
+
 
 export default Home;
